7b751171bc81c76360cf5a480428800568979342
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-02-19 07:36:59 -0300

    chore: Add coverage folder to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -17,10 +17,10 @@ export default {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ['<rootDir>/src/modules/**/services/tests/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,7 @@
+const sum = (number1: number, number2: number): number => {
+  const summed = number1 + number2;
+
+  return summed;
+};
+
+export default sum;
