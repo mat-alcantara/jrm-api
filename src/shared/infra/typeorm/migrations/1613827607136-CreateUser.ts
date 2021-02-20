5a3781f6a1bf35febbe67528b8b747ef74710196
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-02-20 10:52:25 -0300

    chore: Create create-users migration

---
+++ src/shared/infra/typeorm/migrations/1613827607136-CreateUser.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUser1613827607136 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // This allows we to use uuid_generate_v4 on id
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+            enum: ['production', 'sell'],
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
