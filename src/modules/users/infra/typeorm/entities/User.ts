4750bdba6eff8bdef6a173b9269c411a7beeacb8
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-02-20 15:35:00 -0300

    chore: Create User entity

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -1,10 +1,19 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+/* eslint-disable no-shadow */
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+// interface to use in userType column
 enum UserTypes {
   SELL = 'sell',
   PRODUCTION = 'production',
 }
 
+// Entity for all users in the application
 @Entity()
 export default class User {
   @PrimaryGeneratedColumn('uuid')
@@ -19,6 +28,13 @@ export default class User {
   @Column()
   password: string;
 
+  // userType will only allow 'sell' and 'production'
   @Column({ type: 'enum', enum: UserTypes, default: UserTypes.SELL })
   userType: UserTypes;
+
+  @CreateDateColumn('timestamp with time zone')
+  created_at: Date;
+
+  @UpdateDateColumn('timestamp with time zone')
+  updated_at: Date;
 }
