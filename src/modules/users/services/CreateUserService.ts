d8fedf6622fce5eb101e4f3f6d57c6517925c5ae
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-02-21 01:15:44 -0300

    chore: Create CreateUserService

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,7 +1,24 @@
-// import ICreateUserDTO from '../dtos/ICreateUserDTO';
+import { injectable, inject } from 'tsyringe';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '../dtos/ICreateUserDTO';
 
+@injectable()
 export default class CreateUserService {
-  // public async execute(data: ICreateUserDTO): Promise<void> {
-  //   const { name, email, password, userType } = data;
-  // }
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute(data: ICreateUserDTO): Promise<void> {
+    const { name, email, password, userType } = data;
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password,
+      userType,
+    });
+
+    return user;
+  }
 }
