a19766072d38c4a4c39042c445bb8929c73632ee
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-02-21 17:30:43 -0300

    chore: Add beforeEach on tests

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IHashProvider from '@shared/containers/providers/HashProvider/models/IHashProvider';
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
 import User from '../infra/typeorm/entities/User';
 
@@ -9,15 +10,21 @@ export default class CreateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute(data: ICreateUserDTO): Promise<User> {
     const { name, email, password, userType } = data;
 
+    // Hash the password with bcrypt
+    const hashedPassword = await this.hashProvider.createHash(password);
+
     const user = await this.usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword, // Database will have only the hashed password
       userType,
     });
 
--- src/modules/users/services/tests/CreateUserService.spec.ts
+++ src/modules/users/services/tests/CreateUserService.spec.ts
@@ -1,12 +1,25 @@
 import CreateUserService from '@modules/users/services/CreateUserService';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '@shared/containers/providers/HashProvider/fakes/FakeHashProvider';
 
 import UserTypes from '@modules/users/dtos/UserTypes';
 
-const fakeUsersRepository = new FakeUsersRepository();
-const createUserService = new CreateUserService(fakeUsersRepository);
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUserService: CreateUserService;
 
 describe('Create User', () => {
+  // This function will be activated every time a test is created
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+  });
+
+  // Test the creation of a new user
   it('should create a new user', async () => {
     const user = await createUserService.execute({
       name: 'Mateus',
