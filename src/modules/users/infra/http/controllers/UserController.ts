b6023c594410f28828b744766cd5a4690703988a
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-02-22 11:39:40 -0300

    chore: Create UserController

---
+++ src/modules/users/infra/http/controllers/UserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import { container } from 'tsyringe';
+
+export default class UserController {
+  // Create a new user using the CreateUserService
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email, password, userType } = request.body;
+
+    const createUserService = container.resolve(CreateUserService);
+
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+      userType,
+    });
+
+    return response.json(user);
+  }
+}
