41a28ad1fd279a27b9940ab89441e8a482139cf6
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-02-25 07:26:04 -0300

    chore: Create customers entity

---
+++ src/modules/customers/infra/typeorm/entities/Customer.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('customers')
+export default class Customer {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email?: string;
+
+  @Column()
+  area: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
