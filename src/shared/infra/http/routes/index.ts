d9e2dd2aee1f4a1c9567abc1e7b709de0d7da48b
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-02-25 06:54:37 -0300

    chore: Add authRoutes to index.ts

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import userRoutes from '@modules/users/infra/http/routes/users.routes';
+import authRoutes from '@modules/users/infra/http/routes/auth.routes';
 
 const router = Router();
 
 router.use('/', userRoutes);
+router.use('/', authRoutes);
 
 export default router;
