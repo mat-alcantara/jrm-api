006d1a5f3844465a047937819c2e712eb142bc26
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-02-26 13:21:23 -0300

    chore: Finish create in createCustomerService

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -1,4 +1,8 @@
 import { inject, injectable } from 'tsyringe';
+
+import Customer from '@modules/customers/infra/typeorm/entities/Customer';
+
+import ICreateCustomersDTO from '@modules/customers/dtos/CreateCustomerDTO';
 import ICustomersRepository from '../repositories/ICustomersRepository';
 
 @injectable()
@@ -7,4 +11,18 @@ export default class CreateCustomerSession {
     @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
+
+  public async execute(data: ICreateCustomersDTO): Promise<Customer> {
+    const { name, email, area, city, state } = data;
+
+    const customer = await this.customersRepository.create({
+      name,
+      email,
+      area,
+      city,
+      state,
+    });
+
+    return customer;
+  }
 }
