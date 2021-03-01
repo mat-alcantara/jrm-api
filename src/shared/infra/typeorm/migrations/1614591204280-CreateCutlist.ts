ace281e21cc0a0e235878be4bd789911d4c5e6a0
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-01 06:54:55 -0300

    chore: Create cutlist migration

---
+++ src/shared/infra/typeorm/migrations/1614591204280-CreateCutlist.ts
@@ -0,0 +1,121 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+import OrderStoreEnumDTO from '@modules/cutlist/dtos/OrderStoreEnumDTO';
+import PaymentStatusEnumDTO from '@modules/cutlist/dtos/PaymentStatusEnumDTO';
+import OrderStatusEnumDTO from '@modules/cutlist/dtos/OrderStatusEnumDTO';
+
+export default class CreateCutlist1614591204280 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'cutlists',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'customerId',
+            type: 'uuid',
+            isNullable: true,
+          },
+          {
+            name: 'orderStore',
+            type: 'enum',
+            enum: [
+              OrderStoreEnumDTO.FRADE,
+              OrderStoreEnumDTO.JAPUIBA,
+              OrderStoreEnumDTO.SAO_JOAO,
+            ],
+            isNullable: false,
+          },
+          {
+            name: 'paymentStatus',
+            type: 'enum',
+            enum: [
+              PaymentStatusEnumDTO.ORCAMENTO,
+              PaymentStatusEnumDTO.PAGO,
+              PaymentStatusEnumDTO.PARCIAL,
+              PaymentStatusEnumDTO.RECEBER,
+            ],
+            isNullable: false,
+          },
+          {
+            name: 'orderStatus',
+            type: 'enum',
+            enum: [
+              OrderStatusEnumDTO.ENTREGUE,
+              OrderStatusEnumDTO.LIBERADO,
+              OrderStatusEnumDTO.PRODUCAO,
+              OrderStatusEnumDTO.TRANSPORTADO,
+            ],
+            isNullable: false,
+          },
+          {
+            name: 'ps',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'relatedProblems',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'price',
+            type: 'float',
+            isNullable: false,
+          },
+          {
+            name: 'conclusionDate',
+            type: 'date',
+            isNullable: true,
+          },
+          {
+            name: 'cutlist',
+            type: 'array',
+            isArray: true,
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'cutlists',
+      new TableForeignKey({
+        name: 'custlistProvider',
+        columnNames: ['customerId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'customers',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('cutlists', 'cutlistProvider');
+    queryRunner.dropTable('cutlists');
+  }
+}
