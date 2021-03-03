b68d7ab7e23efd9e898a8e525d3efc7ce0a07a7c
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-03 06:53:47 -0300

    chore: Add SpecificMaterialController to materials.routes.ts

--- src/modules/cutlist/infra/http/routes/materials.routes.ts
+++ src/modules/cutlist/infra/http/routes/materials.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
-import MaterialController from '@modules/cutlist/infra/http/controllers/MaterialController';
 import { celebrate, Segments, Joi } from 'celebrate';
+
+import MaterialController from '@modules/cutlist/infra/http/controllers/MaterialController';
+import SpecificMaterialController from '@modules/cutlist/infra/http/controllers/SpecificMaterialController';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const materialRoutes = Router();
+
 const materialController = new MaterialController();
+const specificMaterialController = new SpecificMaterialController();
 
 materialRoutes.use(ensureAuthenticated);
 
 materialRoutes.get('/materials', materialController.show);
+materialRoutes.get('/materials/:id', specificMaterialController.show);
 
 materialRoutes.post(
   '/materials',
