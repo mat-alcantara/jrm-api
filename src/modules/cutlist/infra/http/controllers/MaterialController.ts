3826546cd1fa27970ab77b4a3a0a203a19e008d9
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-03 06:31:06 -0300

    chore: Create show method in MaterialController

--- src/modules/cutlist/infra/http/controllers/MaterialController.ts
+++ src/modules/cutlist/infra/http/controllers/MaterialController.ts
@@ -1,9 +1,20 @@
 import { container } from 'tsyringe';
 
 import CreateMaterialService from '@modules/cutlist/services/CreateMaterialService';
+import ShowAllMaterialsService from '@modules/cutlist/services/ShowAllMaterialsService';
 import { Request, Response } from 'express';
 
 export default class MaterialController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const showAllMaterialsService = await container.resolve(
+      ShowAllMaterialsService,
+    );
+
+    const allMaterials = await showAllMaterialsService.execute();
+
+    return response.json(allMaterials);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const materialData = request.body;
 
