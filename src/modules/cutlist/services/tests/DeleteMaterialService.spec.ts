2d5193630c7a198c71353e89a32d19b4d09153db
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-03 07:11:26 -0300

    chore: Create DeleteMaterialService.spec

---
+++ src/modules/cutlist/services/tests/DeleteMaterialService.spec.ts
@@ -0,0 +1,38 @@
+import FakeMaterialsRepository from '@modules/cutlist/repositories/fakes/FakeMaterialsRepository';
+import CreateMaterialService from '@modules/cutlist/services/CreateMaterialService';
+import DeleteMaterialService from '@modules/cutlist/services/DeleteMaterialService';
+
+import AppError from '@shared/errors/AppError';
+
+let fakeMaterialsRepository: FakeMaterialsRepository;
+let createMaterialService: CreateMaterialService;
+let deleteMaterialService: DeleteMaterialService;
+
+describe('Delete Material', () => {
+  beforeEach(() => {
+    fakeMaterialsRepository = new FakeMaterialsRepository();
+    createMaterialService = new CreateMaterialService(fakeMaterialsRepository);
+    deleteMaterialService = new DeleteMaterialService(fakeMaterialsRepository);
+  });
+
+  it('Should remove a material', async () => {
+    const spyRemoveById = spyOn(fakeMaterialsRepository, 'removeById');
+
+    const materialCreated = await createMaterialService.execute({
+      name: 'MDF COMUM',
+      thickness: 15,
+      width: 2750,
+      height: 1850,
+    });
+
+    await deleteMaterialService.execute(materialCreated.id);
+
+    expect(spyRemoveById).toBeCalledWith(materialCreated.id);
+  });
+
+  it("Should not remove a material if it don't exist", async () => {
+    await expect(
+      deleteMaterialService.execute('wrongId'),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
