5cec792ce72edb93698f8fc36f0b9a2911d06ce3
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-03 07:15:57 -0300

    chore: Add remove to MaterialController

--- src/modules/cutlist/infra/http/controllers/MaterialController.ts
+++ src/modules/cutlist/infra/http/controllers/MaterialController.ts
@@ -1,8 +1,9 @@
+import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateMaterialService from '@modules/cutlist/services/CreateMaterialService';
 import ShowAllMaterialsService from '@modules/cutlist/services/ShowAllMaterialsService';
-import { Request, Response } from 'express';
+import DeleteMaterialService from '@modules/cutlist/services/DeleteMaterialService';
 
 export default class MaterialController {
   public async show(request: Request, response: Response): Promise<Response> {
@@ -25,5 +26,15 @@ export default class MaterialController {
     const materialCreated = await createMaterialService.execute(materialData);
 
     return response.json(materialCreated);
+  }
+
+  public async remove(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteMaterialService = container.resolve(DeleteMaterialService);
+
+    await deleteMaterialService.execute(id);
+
+    return response.json();
   }
 }
