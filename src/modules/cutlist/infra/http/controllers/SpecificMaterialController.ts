fb67423be96a53c0e3a38be98212fd86aed130e3
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-03 06:52:18 -0300

    chore: Create SpecificMaterialController.ts

---
+++ src/modules/cutlist/infra/http/controllers/SpecificMaterialController.ts
@@ -0,0 +1,16 @@
+import { container } from 'tsyringe';
+
+import ShowMaterialById from '@modules/cutlist/services/ShowMaterialByIdService';
+import { Request, Response } from 'express';
+
+export default class SpecificMaterialController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showMaterialById = await container.resolve(ShowMaterialById);
+
+    const specificMaterial = await showMaterialById.execute(id);
+
+    return response.json(specificMaterial);
+  }
+}
