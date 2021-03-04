97afdcd9d5539b2b2407f6c6245c29defa85296f
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-04 06:11:58 -0300

    chore: Create UpdateMaterialService

--- src/modules/cutlist/repositories/IMaterialsRepository.ts
+++ src/modules/cutlist/repositories/IMaterialsRepository.ts
@@ -1,4 +1,5 @@
 import ICreateMaterialDTO from '@modules/cutlist/dtos/ICreateMaterialDTO';
+import IUpdateMaterialDTO from '@modules/cutlist/dtos/IUpdateMaterialDTO';
 import MaterialEntity from '@modules/cutlist/infra/typeorm/entities/MaterialEntity';
 
 export default interface IMaterialsRepository {
@@ -7,4 +8,8 @@ export default interface IMaterialsRepository {
   showAllMaterials(): Promise<MaterialEntity[]>;
   findMaterialById(id: string): Promise<MaterialEntity | undefined>;
   removeById(id: string): Promise<void>;
+  updateMaterial(
+    id: string,
+    updateData: IUpdateMaterialDTO,
+  ): Promise<MaterialEntity>;
 }
---
+++ src/modules/cutlist/services/UpdateMaterialService.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import MaterialEntity from '@modules/cutlist/infra/typeorm/entities/MaterialEntity';
+
+import IMaterialsRepository from '@modules/cutlist/repositories/IMaterialsRepository';
+import IUpdateMaterialDTO from '@modules/cutlist/dtos/IUpdateMaterialDTO';
+
+@injectable()
+export default class UpdateMaterialService {
+  constructor(
+    @inject('MaterialsRepository')
+    private materialsRepository: IMaterialsRepository,
+  ) {}
+
+  public async execute(
+    id: string,
+    updateData: IUpdateMaterialDTO,
+  ): Promise<MaterialEntity> {
+    const materialToUpdate = await this.materialsRepository.findMaterialById(
+      id,
+    );
+
+    if (!materialToUpdate) {
+      throw new AppError('Material does not exist', 404);
+    }
+
+    const updatedMaterial = await this.materialsRepository.updateMaterial(
+      id,
+      updateData,
+    );
+
+    return updatedMaterial;
+  }
+}
