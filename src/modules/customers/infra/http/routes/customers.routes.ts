da510140563103b0133199568b8828099bb4347e
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-04 07:19:16 -0300

    chore: Add get specific customer to customers.routes

--- src/modules/customers/infra/http/routes/customers.routes.ts
+++ src/modules/customers/infra/http/routes/customers.routes.ts
@@ -1,15 +1,19 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import CustomersController from '@modules/customers/infra/http/controllers/CustomersController';
+import SpecificCustomerController from '@modules/customers/infra/http/controllers/SpecificCustomerController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const customersRoutes = Router();
 
 const customersController = new CustomersController();
+const specificCustomerController = new SpecificCustomerController();
 
 customersRoutes.use(ensureAuthenticated); // Ensure all routes after this will need authentication
 
 customersRoutes.get('/customers', customersController.show); // Show all customers
+
+customersRoutes.get('/customers/:id', specificCustomerController.show); // Show a specific customer
 
 customersRoutes.post(
   '/customers',
