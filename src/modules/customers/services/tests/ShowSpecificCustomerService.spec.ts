822b071c9bff631587c7abe7100034b755f9b4a4
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-04 07:15:46 -0300

    chore: Create ShowSpecificCustomerService.spec

---
+++ src/modules/customers/services/tests/ShowSpecificCustomerService.spec.ts
@@ -0,0 +1,43 @@
+import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
+import ShowSpecificCustomerService from '@modules/customers/services/ShowSpecificCustomerService';
+import FakeCustomersRepository from '@modules/customers/repositories/fakes/FakeCustomerRepository';
+
+import AppError from '@shared/errors/AppError';
+
+let fakeCustomersRepository: FakeCustomersRepository;
+let createCustomerService: CreateCustomerService;
+let showSpecificCustomerService: ShowSpecificCustomerService;
+
+describe('Show specific customer', () => {
+  // This function will be activated every time a test is created
+  beforeEach(() => {
+    fakeCustomersRepository = new FakeCustomersRepository();
+    createCustomerService = new CreateCustomerService(fakeCustomersRepository);
+    showSpecificCustomerService = new ShowSpecificCustomerService(
+      fakeCustomersRepository,
+    );
+  });
+
+  it('should be able to show a specific customers', async () => {
+    const customerCreated = await createCustomerService.execute({
+      name: 'Mateus',
+      email: 'mateus@mateus.com',
+      area: 'Frade',
+      telephone: ['24-999710064', '24-999656973'],
+      city: 'Angra dos Reis',
+      state: 'Rio de Janeiro',
+    });
+
+    const specificCustomer = await showSpecificCustomerService.execute(
+      customerCreated.id,
+    );
+
+    await expect(specificCustomer.id).toEqual(customerCreated.id);
+  });
+
+  it('should not be able to show a specific customers if it does not exist', async () => {
+    await expect(
+      showSpecificCustomerService.execute('wrongId'),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
