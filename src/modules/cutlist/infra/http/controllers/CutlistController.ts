ea45579eafb410d0b057cf8b32e9b2e6863720e6
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-05 06:38:47 -0300

    chore: Add remove to CutlistController

--- src/modules/cutlist/infra/http/controllers/CutlistController.ts
+++ src/modules/cutlist/infra/http/controllers/CutlistController.ts
@@ -3,6 +3,7 @@ import { container } from 'tsyringe';
 
 import CreateCutlistService from '@modules/cutlist/services/CreateCutlistService';
 import ShowAllCutlists from '@modules/cutlist/services/ShowAllCutlistsService';
+import DeleteCutlistService from '@modules/cutlist/services/DeleteCutlistService';
 
 export default class MaterialController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -21,5 +22,15 @@ export default class MaterialController {
     const allCutlists = await showAllCutlists.execute();
 
     return response.json(allCutlists);
+  }
+
+  public async remove(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteCutlistService = await container.resolve(DeleteCutlistService);
+
+    await deleteCutlistService.execute(id);
+
+    return response.json();
   }
 }
