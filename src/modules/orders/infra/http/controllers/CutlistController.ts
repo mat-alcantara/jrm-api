20f42d9cec05ef5d151e68792949acc2dd6ff613
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-12 07:25:17 -0300

    chore: Add create to CutlistController

--- src/modules/orders/infra/http/controllers/CutlistController.ts
+++ src/modules/orders/infra/http/controllers/CutlistController.ts
@@ -3,8 +3,20 @@ import { container } from 'tsyringe';
 
 import DeleteCutlistService from '@modules/orders/services/DeleteCutlistService';
 import UpdateCutlistService from '@modules/orders/services/UpdateCutlistService';
+import CreateCutlistService from '@modules/orders/services/CreateCutlistService';
 
 export default class CutlistController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { cutlistData } = request.body;
+
+    const createCutlistService = await container.resolve(CreateCutlistService);
+
+    const orderUpdated = await createCutlistService.execute(id, cutlistData);
+
+    return response.json(orderUpdated);
+  }
+
   public async remove(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
     const { cutlistId } = request.body;
