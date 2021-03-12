3c7193430d6f8226c0a14cec119bdcc1346d6b28
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-12 06:18:34 -0300

    chore: Create UpdateCutlistService.spec

---
+++ src/modules/orders/services/tests/UpdateCutlistService.spec.ts
@@ -0,0 +1,86 @@
+import AppError from '@shared/errors/AppError';
+
+import CreateOrderService from '@modules/orders/services/CreateOrderService';
+import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
+import UpdateCutlistService from '@modules/orders/services/UpdateCutlistService';
+
+import FakeCustomersRepository from '@modules/customers/repositories/fakes/FakeCustomerRepository';
+import FakeOrdersRepository from '@modules/orders/repositories/fakes/FakeOrdersRepository';
+
+import OrderStatusEnumDTO from '@modules/orders/dtos/OrderStatusEnumDTO';
+import OrderStoreEnumDTO from '@modules/orders/dtos/OrderStoreEnumDTO';
+import PaymentStatusEnumDTO from '@modules/orders/dtos/PaymentStatusEnumDTO';
+
+let fakeCustomersRepository: FakeCustomersRepository;
+let createCustomerService: CreateCustomerService;
+let fakeOrdersRepository: FakeOrdersRepository;
+let createOrderService: CreateOrderService;
+let updateCutlistService: UpdateCutlistService;
+
+describe('Update Cutlist', () => {
+  beforeEach(() => {
+    fakeCustomersRepository = new FakeCustomersRepository();
+    createCustomerService = new CreateCustomerService(fakeCustomersRepository);
+    fakeOrdersRepository = new FakeOrdersRepository();
+    createOrderService = new CreateOrderService(
+      fakeOrdersRepository,
+      fakeCustomersRepository,
+    );
+    updateCutlistService = new UpdateCutlistService(fakeOrdersRepository);
+  });
+
+  it('Should update a specific cutlist', async () => {
+    const customerCreated = await createCustomerService.execute({
+      name: 'Mateus',
+      email: 'mateus@mateus.com',
+      area: 'Frade',
+      telephone: ['24-999710064', '24-999656973'],
+      city: 'Angra dos Reis',
+      state: 'Rio de Janeiro',
+    });
+
+    const orderCreated = await createOrderService.execute({
+      customerId: customerCreated.id,
+      orderStatus: OrderStatusEnumDTO.PRODUCAO,
+      orderStore: OrderStoreEnumDTO.FRADE,
+      paymentStatus: PaymentStatusEnumDTO.PARCIAL,
+      price: 215,
+      cutlist: [
+        {
+          id: '',
+          material: 'MDF 15mm Comum',
+          quantidade: 20,
+          side_a_size: 500,
+          side_b_size: 200,
+          side_a_border: 1,
+          side_b_border: 2,
+        },
+        {
+          id: '',
+          material: 'MDF 15mm Ultra',
+          quantidade: 20,
+          side_a_size: 800,
+          side_b_size: 400,
+          side_a_border: 0,
+          side_b_border: 2,
+        },
+      ],
+    });
+
+    const cutlistUpdated = await updateCutlistService.execute(
+      orderCreated.id,
+      orderCreated.cutlist[0].id,
+      { material: 'MDF 15mm Ultra' },
+    );
+
+    await expect(cutlistUpdated.cutlist[0].material).toBe('MDF 15mm Ultra');
+  });
+
+  it('Should not update a specific cutlist if it does not exist', async () => {
+    await expect(
+      updateCutlistService.execute('wrongId', 'wrongCutlistId', {
+        material: 'MDF',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
