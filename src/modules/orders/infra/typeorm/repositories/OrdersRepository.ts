118b62f0b6070d2384f5fa52b62b2d3c3d208535
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-12 06:28:35 -0300

    chore: Add updateCutlist to OrdersRepository

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -4,6 +4,7 @@ import OrderEntity from '@modules/orders/infra/typeorm/entities/OrderEntity';
 
 import IOrdersRepository from '@modules/orders/repositories/IOrdersRepository';
 import ICreateOrderDTO from '@modules/orders/dtos/ICreateOrderDTO';
+import IUpdateCutlistDTO from '@modules/orders/dtos/IUpdateCutlistDTO';
 
 export default class OrdersRepository implements IOrdersRepository {
   private ormRepository: Repository<OrderEntity>;
@@ -49,5 +50,28 @@ export default class OrdersRepository implements IOrdersRepository {
     newOrder.cutlist = orderWithoutCutlist;
 
     await this.ormRepository.save({ ...order, ...newOrder }); // Update a cutlist
+  }
+
+  public async updateCutlist(
+    order: OrderEntity,
+    cutlistId: string,
+    cutlistData: IUpdateCutlistDTO,
+  ): Promise<OrderEntity> {
+    const orderToUpdate = order;
+    const cutlistToUpdate = order.cutlist.find(
+      currentCutlist => currentCutlist.id === cutlistId,
+    );
+
+    if (cutlistToUpdate) {
+      const cutlistUpdated = { ...cutlistToUpdate, ...cutlistData };
+
+      orderToUpdate.cutlist[
+        orderToUpdate.cutlist.indexOf(cutlistToUpdate)
+      ] = cutlistUpdated;
+
+      await this.ormRepository.save(orderToUpdate);
+    }
+
+    return orderToUpdate;
   }
 }
