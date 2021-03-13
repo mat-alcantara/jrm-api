d0e73267097d6553f865964afe32239e67c4c162
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-13 10:15:22 -0300

    chore: Update sentry location

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,27 +3,32 @@ import 'reflect-metadata';
 
 import express, { Response, Request, NextFunction } from 'express';
 import 'express-async-errors'; // Dependency needed to catch erros in the application
-
+import * as Sentry from '@sentry/node';
 import cors from 'cors';
 import helmet from 'helmet';
-
 import 'dotenv/config';
-
 import { errors } from 'celebrate';
 import AppError from '@shared/errors/AppError'; // Import Error instance
-
 import routes from '@shared/infra/http/routes'; // Import all routes
 
 import '@shared/infra/typeorm'; // Import database
 import '@shared/containers/index'; // Import dependency injection Containers
 import '@shared/infra/http/middlewares/RateLimiter';
 
+Sentry.init({
+  dsn:
+    'https://cde455106fad4b979714f3dc28915142@o337250.ingest.sentry.io/5674274',
+  tracesSampleRate: 1.0,
+});
+
 const server = express();
 
+server.use(Sentry.Handlers.requestHandler());
 server.use(cors());
 server.use(helmet());
 server.use(express.json()); // Allow JSON on express
 server.use(routes); // Activate routes on express
+server.use(Sentry.Handlers.errorHandler());
 server.use(errors()); // Celebrate Errors
 
 // Return an error as a instance of AppError
