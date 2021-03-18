d5015777e83b81b154f0ddb32abf84412c3b353c
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-03-18 06:08:42 -0300

    chore: Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,7 @@
 require('dotenv/config');
 
+const rootDir = process.env.NODE_ENV === 'development' ? 'src' : 'dist';
+
 module.exports = {
   type: 'postgres',
   host: process.env.DB_HOST,
@@ -9,8 +11,8 @@ module.exports = {
   database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
-  entities: ['src/modules/**/infra/typeorm/entities/*.ts'],
-  migrations: ['src/shared/infra/typeorm/migrations/*.ts'],
+  entities: [`${rootDir}/modules/**/infra/typeorm/entities/*.ts`],
+  migrations: [`${rootDir}/shared/infra/typeorm/migrations/*.ts`],
   cli: {
     entitiesDir: 'src/modules/**/infra/typeorm/entities',
     migrationsDir: 'src/shared/infra/typeorm/migrations',
