27ad75c37ad1aad8e739f63d4351bafdfc4ffe3e
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-04-21 13:32:16 -0300

    fix: Error in tsconfig

--- dist/shared/containers/providers/AuthProvider/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _JWTAuthProvider = _interopRequireDefault(require("./implementations/JWTAuthProvider"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_tsyringe.container.registerSingleton('AuthProvider', _JWTAuthProvider.default);
--- dist/shared/containers/providers/DateProvider/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _DateFnsDateProvider = _interopRequireDefault(require("./implementations/DateFnsDateProvider"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_tsyringe.container.registerSingleton('DateProvider', _DateFnsDateProvider.default);
