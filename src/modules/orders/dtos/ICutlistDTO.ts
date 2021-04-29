8c6f667651b30fff9064d135109bf3c92c960398
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-04-28 21:50:17 -0300

    chore: Add material_id to ICutlistDTO

--- src/modules/orders/dtos/ICutlistDTO.ts
+++ src/modules/orders/dtos/ICutlistDTO.ts
@@ -1,9 +1,7 @@
-// import MaterialEntity from '@modules/cutlist/infra/typeorm/entities/MaterialEntity';
-
 export default interface ICutlistDTO {
   id: string;
   quantidade: number;
-  material: string;
+  material_id: string;
   side_a_size: number;
   side_b_size: number;
   side_a_border: number;
