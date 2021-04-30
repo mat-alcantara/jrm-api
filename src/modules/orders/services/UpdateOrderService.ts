40c22b5fcbbae0c97f7e6b6aca20269f99a0c880
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-04-30 07:47:36 -0300

    chore: Create UpdateOrderService

--- src/modules/orders/services/UpdateOrderService.ts
+++ src/modules/orders/services/UpdateOrderService.ts
@@ -5,6 +5,8 @@ import IOrdersRepository from '@modules/orders/repositories/IOrdersRepository';
 import IUpdateOrderDTO from '@modules/orders/dtos/IUpdateOrderDTO';
 import OrderEntity from '@modules/orders/infra/typeorm/entities/OrderEntity';
 
+import AppError from '@shared/errors/AppError';
+
 @injectable()
 export default class UpdateOrderService {
   constructor(
@@ -16,9 +18,17 @@ export default class UpdateOrderService {
     orderId: string,
     dataToUpdateOrder: IUpdateOrderDTO,
   ): Promise<OrderEntity> {
+    const orderToUpdated = await this.ordersRepository.findOrderById(orderId);
+
+    if (!orderToUpdated) {
+      throw new AppError('Order does not exist', 404);
+    }
+
     const orderUpdated = await this.ordersRepository.updateOrder(
-      orderId,
+      orderToUpdated,
       dataToUpdateOrder,
     );
+
+    return orderUpdated;
   }
 }
