3de720fef570f7db953b4dac119e8a15f29f6ef5
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-04-30 19:52:43 -0300

    chore: Create migration to add order_code1

---
+++ src/shared/infra/typeorm/migrations/1619822900710-addAutoIncrementIdToOrders.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export default class addAutoIncrementIdToOrders1619822900710
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders',
+      new TableColumn({
+        name: 'order_code',
+        type: 'int',
+        isGenerated: true,
+        generationStrategy: 'increment',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('orders', 'order_code');
+  }
+}
