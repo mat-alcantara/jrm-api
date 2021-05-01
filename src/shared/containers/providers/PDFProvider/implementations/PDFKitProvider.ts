280bf8e029431df6f842bae853826962c3d90b09
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-05-01 08:04:09 -0300

    chore: Finish pdf layout with variables

--- src/shared/containers/providers/PDFProvider/implementations/PDFKitProvider.ts
+++ src/shared/containers/providers/PDFProvider/implementations/PDFKitProvider.ts
@@ -1,14 +1,21 @@
 import fs from 'fs';
 import PDFDocument from 'pdfkit';
 import OrderEntity from '@modules/orders/infra/typeorm/entities/OrderEntity';
+import CustomerEntity from '@modules/customers/infra/typeorm/entities/Customer';
+import MaterialEntity from '@modules/materials/infra/typeorm/entities/MaterialEntity';
 import IPDFProvider from '@shared/containers/providers/PDFProvider/models/IPDFProvider';
 
 export default class PDFKitProvider implements IPDFProvider {
-  public async createPDF(orderToGeneratePDF: OrderEntity): Promise<void> {
+  public async createPDF(
+    orderToGeneratePDF: OrderEntity,
+    customerData: CustomerEntity,
+    materialData: MaterialEntity[],
+  ): Promise<void> {
     const doc = new PDFDocument({ size: 'A4' });
 
     doc.pipe(fs.createWriteStream('output.pdf'));
 
+    // HEADER
     doc
       .font('Times-Bold')
       .fontSize(24)
@@ -23,6 +30,7 @@ export default class PDFKitProvider implements IPDFProvider {
       )
       .moveDown(1.2);
 
+    // Dados do cliente
     doc
       .font('Times-Bold')
       .fontSize(16)
@@ -32,14 +40,14 @@ export default class PDFKitProvider implements IPDFProvider {
     doc
       .font('Courier')
       .fontSize(12)
-      .text('Nome: Mateus Alcantara de Castro', { align: 'left' })
+      .text(`Nome: ${customerData.name}`, { align: 'left' })
       .moveDown(0.3);
 
     doc
       .font('Courier')
       .fontSize(12)
       .text(
-        'Endereço: Travessa dos Coqueiros n° 40, Frade - Angra dos Reis - Rio de Janeiro',
+        `Endereço: ${customerData.street}, ${customerData.area} - ${customerData.city}`,
         { align: 'left' },
       )
       .moveDown(0.3);
@@ -47,9 +55,10 @@ export default class PDFKitProvider implements IPDFProvider {
     doc
       .font('Courier')
       .fontSize(12)
-      .text('Telefone: 24999710064', { align: 'left' })
+      .text(`Telefone: ${customerData.telephone[0]}`, { align: 'left' })
       .moveDown(1);
 
+    // Dados do serviço
     doc
       .font('Times-Bold')
       .fontSize(16)
@@ -61,39 +70,44 @@ export default class PDFKitProvider implements IPDFProvider {
       .fontSize(12)
       .text('Codigo do Serviço: ', { align: 'left', continued: true })
       .font('Courier-Bold')
-      .text('112')
+      .text(`${orderToGeneratePDF.order_code}`)
       .moveDown(0.3);
 
     doc
       .font('Courier')
       .fontSize(12)
-      .text('Vendedor: Mateus Alcantara', { align: 'left' })
+      .text(`Vendedor: ${orderToGeneratePDF.seller}`, { align: 'left' })
       .moveDown(0.3);
 
     doc
       .font('Courier')
       .fontSize(12)
-      .text('Loja do Pedido: Frade', { align: 'left' })
+      .text(`Loja do Pedido: ${orderToGeneratePDF.orderStore}`, {
+        align: 'left',
+      })
       .moveDown(0.3);
 
     doc
       .font('Courier')
       .fontSize(12)
-      .text('Status do Pagamento: Pago', { align: 'left' })
+      .text(`Status do Pagamento: ${orderToGeneratePDF.paymentStatus}`, {
+        align: 'left',
+      })
       .moveDown(0.3);
 
     doc
       .font('Courier')
       .fontSize(12)
-      .text('Observação: Entregar na casa do cliente', { align: 'left' })
+      .text(`Observação: ${orderToGeneratePDF.ps}`, { align: 'left' })
       .moveDown(0.3);
 
     doc
       .font('Courier')
       .fontSize(12)
-      .text('Valor: R$ 600,00', { align: 'left' })
+      .text(`Valor: ${orderToGeneratePDF.price}`, { align: 'left' })
       .moveDown(1);
 
+    // Dados das peças
     doc
       .font('Times-Bold')
       .fontSize(16)
@@ -108,13 +122,18 @@ export default class PDFKitProvider implements IPDFProvider {
       })
       .moveDown(0.4);
 
-    doc
-      .font('Courier')
-      .fontSize(12)
-      .text('5 - MDF Comum 15mm - 400 [2] x 200 [4] - R$ 200,00', {
-        align: 'left',
-      })
-      .moveDown(0.2);
+    for (let i = 0; i < orderToGeneratePDF.cutlist.length; i += 1) {
+      doc
+        .font('Courier')
+        .fontSize(12)
+        .text(
+          `${orderToGeneratePDF.cutlist[i].quantidade} - ${materialData[i].name} - ${orderToGeneratePDF.cutlist[i].side_a_size} [${orderToGeneratePDF.cutlist[i].side_a_border}] x ${orderToGeneratePDF.cutlist[i].side_b_size} [${orderToGeneratePDF.cutlist[i].side_b_border}] - R$ ${orderToGeneratePDF.cutlist[i].price}`,
+          {
+            align: 'left',
+          },
+        )
+        .moveDown(0.2);
+    }
 
     doc
       .font('Courier')
--- src/shared/containers/providers/PDFProvider/models/IPDFProvider.ts
+++ src/shared/containers/providers/PDFProvider/models/IPDFProvider.ts
@@ -1,5 +1,11 @@
 import OrderEntity from '@modules/orders/infra/typeorm/entities/OrderEntity';
+import CustomerEntity from '@modules/customers/infra/typeorm/entities/Customer';
+import MaterialEntity from '@modules/materials/infra/typeorm/entities/MaterialEntity';
 
 export default interface IPDFProvider {
-  createPDF(orderToGeneratePDF: OrderEntity): Promise<void>;
+  createPDF(
+    orderToGeneratePDF: OrderEntity,
+    customerData: CustomerEntity,
+    materialData: MaterialEntity[],
+  ): Promise<void>;
 }
