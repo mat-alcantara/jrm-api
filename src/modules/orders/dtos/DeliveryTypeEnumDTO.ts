bac1d5ae9ee99b76d3dcdfef7b00153a40491be2
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-05-12 12:25:57 -0300

    chore: Create Migration

---
+++ src/modules/orders/dtos/DeliveryTypeEnumDTO.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-shadow
+enum DeliveryType {
+  ENTREGA = 'Entrega',
+  LOJA = 'Retirar na Loja',
+}
+
+export default DeliveryType;
---
+++ src/shared/infra/typeorm/migrations/1620832784602-addDeliveryTypeToOrders.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+import DeliveryTypeEnumDTO from '@modules/orders/dtos/DeliveryTypeEnumDTO';
+
+export default class addDeliveryTypeToOrders1620832784602
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders',
+      new TableColumn({
+        name: 'delivery_type',
+        type: 'enum',
+        enum: [DeliveryTypeEnumDTO.ENTREGA, DeliveryTypeEnumDTO.LOJA],
+        isNullable: false,
+        default: DeliveryTypeEnumDTO.LOJA,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('orders', 'delivery_type');
+  }
+}
