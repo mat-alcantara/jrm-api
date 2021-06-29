e1f48b94afba15ef8cc96c43074371e72b6ca5bb
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-06-29 06:10:42 -0300

    chore: Create updateDeliveryService method without given date

---
+++ src/modules/orders/services/updateDeliveryDateService.ts
@@ -0,0 +1,50 @@
+// Recebe o id de um pedido
+// Recebe uma data de entrega (opcional)
+// Muda a data de entrega do pedido
+
+/* eslint-disable no-param-reassign */
+import 'reflect-metadata';
+import { inject, injectable } from 'tsyringe';
+import ff from 'date-fns';
+
+import OrderEntity from '@modules/orders/infra/typeorm/entities/OrderEntity';
+
+import IOrdersRepository from '@modules/orders/repositories/IOrdersRepository';
+import IDateProvider from '@shared/containers/providers/DateProvider/models/IDateProvider';
+
+import AppError from '@shared/errors/AppError';
+
+@injectable()
+export default class UpdateDeliveryDateService {
+  constructor(
+    @inject('OrdersRepository')
+    private ordersRepository: IOrdersRepository,
+
+    @inject('DateProvider')
+    private dateProvider: IDateProvider,
+  ) {}
+
+  public async execute(id: string, deliveryDate?: Date): Promise<OrderEntity> {
+    const orderToUpdateDeliveryDate = await this.ordersRepository.findOrderById(
+      id,
+    );
+
+    if (!orderToUpdateDeliveryDate) {
+      throw new AppError('Order does not exist', 404);
+    }
+
+    if (deliveryDate) {
+      return orderToUpdateDeliveryDate;
+    }
+    const newDate = await this.dateProvider.defaultDate7Days();
+
+    const orderUpdated = await this.ordersRepository.updateOrder(
+      orderToUpdateDeliveryDate,
+      {
+        deliveryDate: newDate,
+      },
+    );
+
+    return orderUpdated;
+  }
+}
